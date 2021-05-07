74bd00f8613c1573abd241a4e1bfa2525162707b
Kubus <lukas.kubik@posteo.net>
2021-05-07 21:19:11 +0200

    Removing unnecessary echo

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ python -m pip install -r requirements.txt
 # setup systemd service
 cp simple_server_mon.template simple_server_mon.service
 working_dir=$(echo $PWD)
-echo $working_dir
 sed -i "s+script_home+$working_dir+g" "simple_server_mon.service"
 sudo cp simple_server_mon.service /etc/systemd/system
 sudo systemctl daemon-reload
